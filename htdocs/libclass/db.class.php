b9e4713dd57f0927c918fbf0cc9cf14b475e0638
chgresma <chgresma@gmail.com>
2021-01-29 18:15:01 +0700

    newly added DB class file

---
+++ htdocs/libclass/db.class.php
@@ -0,0 +1,20 @@
+<?php
+
+    /// Database class file configuration and its methods
+    class DB{
+        ///  make this private to be not access out of this class file
+        private static function config(){
+
+        }
+
+        ///  make this private to be not access out of this class file
+        private static function connect(){ 
+
+        }
+
+        /// fetch data on selected table from database
+        public static function fetchData(){
+
+        }
+    }
+?>
